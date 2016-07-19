3310373d7f9a9a3caad28da8a70d9470cbf8a699
Trzebiatowski <trzjakub@amazon.com>
2016-07-19 10:45:38 +0000

    taffybar.sh

---
+++ taffybar.sh
@@ -0,0 +1,5 @@
+sudo apt-get install alex happy
+sudo apt-get install libgtk2.0-dev libpango1.0-dev libglib2.0-dev libcairo2-dev
+cabal install gtk2hs-buildtools
+cabal install gtk
+cabal install taffybar
